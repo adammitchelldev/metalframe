c6b6d0cf95902bafc03f0ad6b2f7bd82fd1fbb6f
Adam Mitchell <isogash@dev.cookieslap.net>
2014-02-27 21:46:16 +0000

    Adding what I did already

---
+++ deathphaser.lua
@@ -0,0 +1,21 @@
+deathphaser = {} -- create our own table to store global functions (so we don't overwrite anything)
+
+local function onInit()
+    pigbones.setItemData("phaserGun","aimRange",2000)
+	pigbones.setItemData("phaserGun","alwaysAim",true)
+	pigbones.setAttackData("physical","damage",0)
+	pigbones.setAttackData("physical","useImpulseDamageModifier",false)
+	ACTIONS.item.getters.isType.parameters.itemType.table.phaserGun.ai.attackDist = 2000
+	ACTIONS.item.getters.isType.parameters.itemType.table.phaserGun.ai.attackArc = 0.03
+	ACTIONS.item.getters.isType.parameters.itemType.table.phaserGun.ai.dangerFactor = 10
+	pigbones.addhook(pigbones.itemDataLocation..".phaserGun.useMethod", deathphaser.onPhaser, "post")
+end
+
+function deathphaser.onPhaser (values,args) -- our recoil function (which is called when a gun has finished firing)
+	if(values[1] == true) then -- if the gun actually fired
+		args[2]:propel(args[3],200) -- propel the shooter in the direction of the new angle a proportion of the gun's real recoil
+	end
+	return values -- return the values so that the game can still use them VERY IMPORTANT
+end
+
+hook.add("gameInit", onInit) -- call onInit when the game starts
---
+++ pigbones.lua
@@ -0,0 +1,102 @@
+pigbones = {}
+pigbones._oldFuncs = {}
+pigbones._hookFuncs = {}
+pigbones._preHookTables = {}
+pigbones._postHookTables = {}
+pigbones.itemDataLocation = "ACTIONS.item.getters.isType.parameters.itemType.table"
+--local function onInit ()
+
+--end
+
+--item.def.useMethod ( I think )
+--parameters 1:(table)item 2:(table)actor 3:(number)angle 4:(number)unknown
+--returns 1:(boolean)success
+--Actor.propel(angle, force)
+--Actor.applyForce(angle,force)
+
+
+function pigbones.setItemData (item, key, value)
+	ACTIONS.item.getters.isType.parameters.itemType.table[item][key] = value 
+end
+
+function pigbones.setAttackData (attack, key, value)
+	ATTACKS[attack][key] = value
+end
+
+function pigbones.addhook (target, callback, hooktype)
+	if pigbones._oldFuncs[target] == nil then -- we need to create the hook first time round
+		pigbones._oldFuncs[target] = pigbones.getfield(target) -- store the old function
+		pigbones._hookFuncs[target] = function (...) -- create the new function
+			local args = pigbones._prehook(target, {...}) -- call all of the prehooks
+			if(args == nil) then return end -- a nil return means the 'event' was cancelled
+			local values = {pigbones._oldFuncs[target](unpack(args))} -- call the original function
+			return unpack(pigbones._posthook(target, values, args)) -- call all of the post hooks
+			--return false
+		end
+		pigbones.setfield(target, pigbones._hookFuncs[target]) -- set the new function to be called
+		pigbones._preHookTables[target] = {n = 0} -- setup pre hook storage
+		pigbones._postHookTables[target] = {n = 0} -- setup post hook storage
+	end
+	if hooktype == nil or callback == nil then return end
+	if hooktype == "pre" then
+		pigbones._preHookTables[target].n = pigbones._preHookTables[target].n + 1
+		pigbones._preHookTables[target][pigbones._preHookTables[target].n] = callback
+	elseif hooktype == "post" then
+		--print("callback added")
+		pigbones._postHookTables[target].n = pigbones._postHookTables[target].n + 1
+		pigbones._postHookTables[target][pigbones._postHookTables[target].n] = callback
+	end
+end
+
+function pigbones._prehook (target, args)
+	for i,v in ipairs(pigbones._preHookTables[target]) do
+		args = v(args)
+		if args == nil then return nil end
+	end
+	return args
+end
+
+function pigbones._posthook (target, values, args)
+	--print("posthook")
+	--print(target)
+	for k,v in pairs(pigbones._postHookTables[target]) do
+		if k ~= "n" then
+			--print("callback")
+			values = v(values, args)
+		end
+	end
+	return values
+end
+
+function pigbones.getfield (f)
+	local v = _G    -- start with the table of globals
+	for w in string.gfind(f, "[%w_]+") do
+		v = v[w]
+	end
+	return v
+end
+
+function pigbones.setfield (f, v)
+	local t = _G    -- start with the table of globals
+	for w, d in string.gfind(f, "([%w_]+)(.?)") do
+		if d == "." then      -- not last field?
+			t[w] = t[w] or {}   -- create table if absent
+			t = t[w]            -- get the table
+		else                  -- last field
+			t[w] = v            -- do the assignment
+		end
+	end
+end
+
+function pigbones.recursivePrint (values, indent, level)
+	level = level or 1
+	indent = indent or ""
+	for k,v in pairs(values) do
+		print(indent..tostring(k)..":"..tostring(v))
+		if(type(v) == "table" and level < 2) then
+			pigbones.recursivePrint(v, indent.."    ", level + 1)
+		end
+	end
+end
+	
+--hook.add("gameInit", onInit)
