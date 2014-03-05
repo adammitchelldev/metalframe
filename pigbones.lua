1329c56405304b09704dc77f72ab9d2a1a06fb2f
Adam Mitchell <isogash@dev.cookieslap.net>
2014-03-05 18:12:45 +0000

    got rid of unnecessary stuff

--- pigbones.lua
+++ pigbones.lua
@@ -3,7 +3,6 @@ pigbones._oldFuncs = {}
 pigbones._hookFuncs = {}
 pigbones._preHookTables = {}
 pigbones._postHookTables = {}
-pigbones.itemDataLocation = "ACTIONS.item.getters.isType.parameters.itemType.table"
 --local function onInit ()
 
 --end
@@ -14,13 +13,23 @@ pigbones.itemDataLocation = "ACTIONS.item.getters.isType.parameters.itemType.tab
 --Actor.propel(angle, force)
 --Actor.applyForce(angle,force)
 
-
-function pigbones.setItemData (item, key, value)
-	ITEMS[item][key] = value 
-end
-
-function pigbones.setAttackData (attack, key, value)
-	ATTACKS[attack][key] = value
+--From Mr_TP's weapon pack
+function pigbones.deepcopy(object)
+    local lookup_table = {}
+    local function _copy(object)
+        if type(object) ~= "table" then
+            return object
+        elseif lookup_table[object] then
+            return lookup_table[object]
+        end
+        local new_table = {}
+        lookup_table[object] = new_table
+        for index, value in pairs(object) do
+            new_table[_copy(index)] = _copy(value)
+        end
+        return setmetatable(new_table, _copy(getmetatable(object)))
+    end
+    return _copy(object)
 end
 
 function pigbones.addhook (target, callback, hooktype)
