9d839cefe03d901f815483b085de270cd659df7d
Adam Mitchell <isogash@dev.cookieslap.net>
2014-03-11 21:16:32 +0000

    changed events to be class based registering a new event is like creating a new instance of the _mf.event.Instance class this saves on having to store new functions for every new event, being much more memory efficient Catlinman, you may want to add some more functionality to this so it is more consistent with how you can access it

--- lua/mfevents.lua
+++ lua/mfevents.lua
@@ -8,43 +8,71 @@ _mf.events = {}
 -- We make the hooks table local since we don't want the user to have direct access to them.
 -- Each mod is added to one of these tables if it requires a hook callback.
 
-function _mf.event.registerEvent(eventname)
-	if not _mf.events[eventname] then
-		local e = {}
+_mf.event.Instance = {}
+_mf.event.Instance_mt = { __index = _mf.event.Instance }
 
-		e.callbacks = {}
+function _mf.event.Instance:new(eventname)
+	local e = {}
+	setmetatable(e, _mf.event.Instance_mt)
+	e.callbacks = {
+		[1] = {}
+		[2] = {}
+		[3] = {}
+		[4] = {}
+		[5] = {}
+	}
+	e.name = eventname
+	e.active = true
+	_mf.events[eventname] = e
+	return e
+end
 
-		function e:fire()
-			for i, c in pairs(self.callbacks) do
-				c()
+function _mf.event.Instance:pop()
+	self = nil
+end
+
+function _mf.event.Instance:fire()
+	if not self.active return nil
+	for i, p in ipairs(self.callbackPriorities) do
+		for k, c in ipairs(p) do
+			if c then
+				c.fire()
+			else
+				table.remove(p, k)
 			end
 		end
-
-		_mf.events[eventname] = e
 	end
+end
+
+function _mf.event.Instance:registerCallback(callbackname, callback, priority)
+	priority = priority or 3
+	if self.callbackPriorities[priority] then
+		local callbackInstance = self.callbacks[callbackname]
+		if not callbackInstance then
+			self.callbacks[callbackname] = {}
+			callbackInstance = self.callbacks[callbackname]
+			callbackInstance.active = true
+		end
+		callbackInstance.fire = callback
+		callbackInstance.priority = priority
+		table.insert(self.callbackPriorities[priority], callbackInstance)
+		return callbackInstance
+	end
+end
+
+function _mf.event.registerEvent(eventname)
+	return _mf.event.Instance:new(eventname)
 end
 
 function _mf.event.popEvent(eventname)
 	if _mf.events[eventname] then
-		_mf.events[eventname] = nil
+		_mf.events[eventname]:pop()
 	end
 end
 
-function _mf.event.registerCallback(eventname, callback)
-	if not _mf.events[eventname] then
-
-		-- Better to do this with table.insert
-		if not _mf.events[eventname].callbacks[callback] then
-			_mf.events[eventname].callbacks[callback] = callback
-		end
-	end
-end
-
-function _mf.event.popCallback(eventname, callback)
+function _mf.event.popCallback(eventname, callbackname)
 	if _mf.events[eventname] then
-		if not _mf.events[eventname].callbacks[callback] then
-			_mf.events[eventname].callbacks[callback] = nil
-		end
+		_mf.events[eventname]:popCallback(callbackname)
 	end
 end
 
